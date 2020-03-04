5e280728854523172a81be7cc49afd7223a00aab
yoo <yoo@DESKTOP-CPQVAH5>
2020-03-04 17:26:38 +0900

    tert

--- gittest/src/gittest/test.java
+++ gittest/src/gittest/test.java
@@ -3,7 +3,7 @@ package gittest;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("hello java");
-		
+		System.out.println("hello java");
 		System.out.println("hello java");System.out.println("hello java");
 	}
 }
